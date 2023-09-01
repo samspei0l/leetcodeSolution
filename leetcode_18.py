db20870027d9f8946a8c039084b31c2f87938a87
samspei0l <omkaryash123@gmail.com>
2023-09-01 10:59:51 +0545

    Adding TC and SC with Explaination of Solution

--- leetcode_18.py
+++ leetcode_18.py
@@ -1,3 +1,12 @@
+'''
+Brute Force: Running for loops for every i , j, k and l.
+If theri sum == target then store them in temp and sort the temp
+Add the values of temp in set DS for distinct item.
+append the items in set to ans of type list. 
+TC - O(N^4) ans SC - O(1)
+'''
+
+
 def leetcode18Brute(nums, target):
     n = len(nums)
     ans = []
@@ -12,6 +21,18 @@ def leetcode18Brute(nums, target):
                         distinc.add(tuple(temp))
     ans = [list(item) for item in distinc]
     print(ans)
+
+
+'''
+Better Solution: We declear a set DS and a list ans. 
+Run for loops for i and j and k.
+Decleare another set to be used after j. 
+We know if we do target - nums[i] + nums[j] + nums[k] we will get the fourth element.
+If fourth element is in hashset then add nums[i], nums[j], nums[k], fourth in temp.
+Sort the temp and add the values in distinct set DS. If fourth not in hashset then
+add the value nums[k] to hashset. At last append the items of distinct to list of ans.
+TC - O(N^3*logM) SC- O(2 * No of quadruples)+ O(N)
+'''
 
 
 def leetcode18Better(nums, target):
@@ -32,6 +53,23 @@ def leetcode18Better(nums, target):
                 hashset.add(nums[k])
     ans = list(distinct)
     print(ans)
+
+
+'''
+Optimal Solution: Two Pointer Approach
+Here we fix i and j and we move k and l throught the array. 
+First we will sort the array.
+Then we will run a loop for i and check if i != 0 and previous element != to current element.
+Similarly we will run a loop for j and check if j > i+1 or not and if previous element at j == current element at j
+We define our k as j + 1 because it will start from the index after j and l as len(array) - 1 as it will start at the end of array.
+While k < l: We find the sum of all the array i.e nums[i], nums[j], nums[k] and nums[l]
+If the given sum is > target we decrease our l. If the sum < target we increase our k.
+If the sum == target then we add the values to temp. and increase our k and decrease our l for next iteration.
+At last we check while k < l and previous element of k is equal to current element of k then we increase k by 1
+and Similarly we check while k < l and previous element of l is equal to current element of l then we decrease l by 1.
+At last we append the values of temp in list of ans and return the ans. 
+TC - O(N^3) and SC - O(no of quadruples)
+'''
 
 
 def leetcode18Optimal(nums, target):
