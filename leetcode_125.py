a18c111d8041eebffee18e22b100a8cb02763d5b
samspei0l <omkaryash123@gmail.com>
2023-09-05 13:40:00 +0545

    Leetcode Solution Valid Palindrome String

--- leetcode_125.py
+++ leetcode_125.py
@@ -1,28 +1,40 @@
-def leetcode125(s):
-    n = len(s)
-<<<<<<< HEAD
-    s.lower()
+'''
+Optimal Solution: We can use two method One is to use inbult python function to check
+the string is alphanumeric or not and then compairing the lower case of normal string and
+it's reverse. If they are equal for every character then they are palindrome if not they are not
+palindrome. Another method is to use Two Pointer method. Before doing that we need to check if the given string is alphaNumeric or not.
+To check the string is alphaNumeric we can use python ord() function. To start our two pointer approach, we will use
+left and right pointer pointing at 0 and len(string) - 1. if the lower case of both left and right pointer character is not equal then return False else return True.
+Increase the left by 1 and decrease the right by -1. Once left crosses the right we can know the given string is palindrome or not
+'''
 
-=======
-    reverse = s[::-1]
-    s = s.replace(",", "")
-    s = s.replace(":","")
-    print(s)
-    print(reverse.lower())
-    if (s.lower() == reverse.lower()):
+
+def leetcode125(self, s):
+    left, right = 0, len(s) - 1
+    while left < right:
+        while left < right and not self.alphaNums(s[left]):
+            left += 1
+        while right > left and not self.alphaNums(s[right]):
+            right -= 1
+        if s[left].lower() != s[right].lower():
+            print("False")
+        left += 1
+        right -= 1
         print("True")
-    else: print("False")
->>>>>>> b7c13ef57b3d25cbc776e8d40d64aa1a2d506a9f
 
-def main():
-    s = "A man, a plan, a canal: Panama"
-    leetcode125(s)
 
-<<<<<<< HEAD
+def alphaNums(self, c):
+    return (
+        (ord("A") <= ord(c) <= ord("Z")) or
+        (ord("a") <= ord(c) <= ord("z")) or
+        (ord("0") <= ord(c) <= ord("9"))
+    )
 
-if __name__ == "__main__":
-    main()
-=======
-if __name__ == "__main__":
-    main()
->>>>>>> b7c13ef57b3d25cbc776e8d40d64aa1a2d506a9f
+
+class Solution:
+    def main(self):
+        s = "A man, a plan, a canal: Panama"
+        leetcode125(self, s)
+
+    if __name__ == "__main__":
+        main()
